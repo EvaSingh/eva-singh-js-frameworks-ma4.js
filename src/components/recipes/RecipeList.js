b147f0be1ea0b67b002b959a426697d53033e54c
Eva Singh <eva.singh@hotmail.com>
2020-12-06 11:33:56 +0100

    Install React Bootstrap and fetch recipies array from api.

--- src/components/recipes/RecipeList.js
+++ src/components/recipes/RecipeList.js
@@ -1,14 +1,33 @@
-import React from "react";
-import RecipeItem from "./RecipeItem";
-import SearchRecipe from "./RecipeItem";
+import React, { useState, useEffect }from "react";
+import Spinner from "react-bootstrap/Spinner";
+import { BASE_URL } from "../../constants/api";
+//import RecipeItem from "./RecipeItem";
+//import SearchRecipe from "./SearchRecipe";
 
 export function RecipeList() {
-    return (
-      <>
-        <RecipeItem></RecipeItem>
-        <SearchRecipe></SearchRecipe>
-      </>    
-    );
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(response => response.json())  
+      .then(json => setRecipes(json.results))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false));
+
+  }, []);
+
+  if (loading) {
+    return <Spinner animation="border" className="spinner" />
+  }
+
+  return (
+    <ul>
+      {recipes.map((recipe, index) => (
+        <li key={index}>{recipe.title}</li>
+      ))}
+    </ul>    
+  );
 }
 
 export default RecipeList;
